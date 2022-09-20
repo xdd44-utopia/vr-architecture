853a42e5c84963fa56db9361c8151f38cd4d6607
XDD44 <50290408+xdd44-utopia@users.noreply.github.com>
2022-09-19 23:25:15 -0400

    revise scaling

--- Assets/Scripts/Block/BlockController.cs
+++ Assets/Scripts/Block/BlockController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -84,6 +85,7 @@ public class BlockController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+
 		if (bt != BlockType.furniture) {
 			originalMat.color = new Color(originalMat.color.r, originalMat.color.g, originalMat.color.b, blockTransform.localScale.y > 2f ? 0.4f : 0.9f);
 			GetComponent<Renderer>().material = originalMat;
@@ -148,6 +150,17 @@ public class BlockController : MonoBehaviour
 					break;
 			}
 		}
+		
+		Vector3 leftInitPos = GestureHandler.leftHandInitPos;
+		Vector3 rightInitPos = GestureHandler.rightHandInitPos;
+		Vector3 leftPos = GestureHandler.leftHandPos;
+		Vector3 rightPos = GestureHandler.rightHandPos;
+		if (!isReal) {
+			leftPos = blockTransform.InverseTransformDirection(leftPos);
+			rightPos = blockTransform.InverseTransformDirection(rightPos);
+			leftInitPos = blockTransform.InverseTransformDirection(leftInitPos);
+			rightInitPos = blockTransform.InverseTransformDirection(rightInitPos);
+		}
 
 		//Scale
 		hasRotated = false;
@@ -155,51 +168,59 @@ public class BlockController : MonoBehaviour
 			((GestureHandler.leftTriggerDown && GestureHandler.rightTriggerDown) ||
 			(GestureHandler.leftTriggerPressed && GestureHandler.rightTriggerDown) ||
 			(GestureHandler.leftTriggerDown && GestureHandler.rightTriggerPressed)) &&
-			leftHandInside() && rightHandInside() &&
+			(leftHandInside() || rightHandInside()) &&
 			StatusRecord.tool == StatusRecord.ControllerStatus.BlockControl
 		) {
-			if (bt == BlockType.wall && !isLofi) {
-				scaleDir = isRotated ? Direction.z : Direction.x;
+			if (bt == BlockType.wall) {
+				scaleDir = Direction.z;
 			}
 			else if (
-				Mathf.Abs(GestureHandler.leftHandPos.x - GestureHandler.rightHandPos.x) > Mathf.Abs(GestureHandler.leftHandPos.y - GestureHandler.rightHandPos.y) &&
-				Mathf.Abs(GestureHandler.leftHandPos.x - GestureHandler.rightHandPos.x) > Mathf.Abs(GestureHandler.leftHandPos.z - GestureHandler.rightHandPos.z)
+				Mathf.Abs(leftPos.x - rightPos.x) > Mathf.Abs(leftPos.y - rightPos.y) &&
+				Mathf.Abs(leftPos.x - rightPos.x) > Mathf.Abs(leftPos.z - rightPos.z)
 			) {
-				scaleDir = !isRotated ? Direction.x : Direction.z;
+				scaleDir = !isReal ? Direction.x : !isRotated ? Direction.x : Direction.z;
 			}
 			else if  (
-				(Mathf.Abs(GestureHandler.leftHandPos.z - GestureHandler.rightHandPos.z) > Mathf.Abs(GestureHandler.leftHandPos.y - GestureHandler.rightHandPos.y) &&
-				Mathf.Abs(GestureHandler.leftHandPos.z - GestureHandler.rightHandPos.z) > Mathf.Abs(GestureHandler.leftHandPos.x - GestureHandler.rightHandPos.x)) ||
+				(Mathf.Abs(leftPos.z - rightPos.z) > Mathf.Abs(leftPos.y - rightPos.y) &&
+				Mathf.Abs(leftPos.z - rightPos.z) > Mathf.Abs(leftPos.x - rightPos.x)) ||
 				bt == BlockType.plane || bt == BlockType.floor
 			) {
-				scaleDir = !isRotated ? Direction.z : Direction.x;
+				scaleDir = !isReal ? Direction.z : !isRotated ? Direction.z : Direction.x;
 			}
 			else {
 				scaleDir = Direction.y;
 			}
 			initScale = blockTransform.localScale;
 		}
-		if (GestureHandler.leftTriggerPressed && GestureHandler.rightTriggerPressed && leftHandInside() && rightHandInside() &&
+		if (GestureHandler.leftTriggerPressed && GestureHandler.rightTriggerPressed && (leftHandInside() || rightHandInside()) &&
 			StatusRecord.tool == StatusRecord.ControllerStatus.BlockControl &&
 			(StatusRecord.currentBlock == blockID || StatusRecord.currentBlock == -1)
 		) {
 			float scalar = 1;
 			switch (scaleDir) {
 				case Direction.x:
-					scalar = !isRotated ?
-						(GestureHandler.leftHandPos.x - GestureHandler.rightHandPos.x) / (GestureHandler.leftHandInitPos.x - GestureHandler.rightHandInitPos.x) :
-						(GestureHandler.leftHandPos.z - GestureHandler.rightHandPos.z) / (GestureHandler.leftHandInitPos.z - GestureHandler.rightHandInitPos.z);
+					scalar = !isReal ? (leftPos.x - rightPos.x) / (leftInitPos.x - rightInitPos.x) :
+						(!isRotated ?
+						(leftPos.x - rightPos.x) / (leftInitPos.x - rightInitPos.x) :
+						(leftPos.z - rightPos.z) / (leftInitPos.z - rightInitPos.z));
 					break;
 				case Direction.y:
-					scalar = (GestureHandler.leftHandPos.y - GestureHandler.rightHandPos.y) / (GestureHandler.leftHandInitPos.y - GestureHandler.rightHandInitPos.y);
+					scalar = (leftPos.y - rightPos.y) / (leftInitPos.y - rightInitPos.y);
 					break;
 				case Direction.z:
-					scalar = !isRotated ?
-						(GestureHandler.leftHandPos.z - GestureHandler.rightHandPos.z) / (GestureHandler.leftHandInitPos.z - GestureHandler.rightHandInitPos.z) :
-						(GestureHandler.leftHandPos.x - GestureHandler.rightHandPos.x) / (GestureHandler.leftHandInitPos.x - GestureHandler.rightHandInitPos.x);
+					scalar = !isReal ? (leftPos.z - rightPos.z) / (leftInitPos.z - rightInitPos.z) :
+						(!isRotated ?
+						(leftPos.z - rightPos.z) / (leftInitPos.z - rightInitPos.z) :
+						(leftPos.x - rightPos.x) / (leftInitPos.x - rightInitPos.x));
 					break;
 			}
+			if (Single.IsNaN(scalar)) {
+				scalar = 1;
+			}
 			scalar = Mathf.Clamp(scalar, 0.2f, 5f);
+			float debugFloat = scalar;
+			Vector3 debugVector1 = blockTransform.localScale;
+			Vector3 debugVector2 = initScale;
 			blockTransform.localScale = new Vector3(
 				scaleDir == Direction.x ? initScale.x * scalar : initScale.x,
 				scaleDir == Direction.y ? initScale.y * scalar : initScale.y,
@@ -233,9 +254,6 @@ public class BlockController : MonoBehaviour
 					colorMenuHifi.enableMenu(this);
 				}
 			}
-			else {
-				Debug.Log("Failed");
-			}
 		}
 
 	}
@@ -251,13 +269,14 @@ public class BlockController : MonoBehaviour
 		return isInside(GestureHandler.rightHandPos);
 	}
 	private bool isInside(Vector3 pos) {
+		pos = blockTransform.InverseTransformPoint(pos);
 		return
-			pos.x < blockTransform.position.x + (!isRotated ? maxX : maxZ) * (!isRotated ? blockTransform.localScale.x : blockTransform.localScale.z) &&
-			pos.x > blockTransform.position.x - (!isRotated ? maxX : maxZ) * (!isRotated ? blockTransform.localScale.x : blockTransform.localScale.z) &&
-			pos.y < blockTransform.position.y + maxY * blockTransform.localScale.y &&
-			pos.y > blockTransform.position.y - maxY * blockTransform.localScale.y &&
-			pos.z < blockTransform.position.z + (!isRotated ? maxZ : maxX) * (!isRotated ? blockTransform.localScale.z : blockTransform.localScale.x) &&
-			pos.z > blockTransform.position.z - (!isRotated ? maxZ : maxX) * (!isRotated ? blockTransform.localScale.z : blockTransform.localScale.x);
+			pos.x < maxX &&
+			pos.x > - maxX &&
+			pos.y < maxY &&
+			pos.y > - maxY &&
+			pos.z < maxZ &&
+			pos.z > - maxZ;
 	}
 
 	public void calcBoundingBox() {
@@ -272,20 +291,20 @@ public class BlockController : MonoBehaviour
 		}
 		switch (bt) {
 			case BlockType.plane:
-				maxY = 0.2f;
+				maxY = isReal ? 0.2f : 1f;
 				break;
 			case BlockType.floor:
-				maxY = 0.2f;
+				maxY = isReal ? 0.2f : 1f;
 				break;
 			case BlockType.wall:
-				maxX = 0.2f;
+				maxX = isReal ? 0.2f : 1f;
 				break;
 		}
-		if (!isReal) {
-			maxX /= 10;
-			maxY /= 10;
-			maxZ /= 10;
-		}
+		// if (!isReal) {
+		// 	maxX /= 10;
+		// 	maxY /= 10;
+		// 	maxZ /= 10;
+		// }
 		maxX *= 1.01f;
 		maxY *= 1.01f;
 		maxZ *= 1.01f;
